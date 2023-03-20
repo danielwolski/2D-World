0500c25561c1d83f6d41ac08c5fc028352f8992d
danielwolski <80836050+danielwolski@users.noreply.github.com>
2023-03-20 14:10:58 +0100

    Upload initial files

---
+++ PO_PROJECT1/Main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <ctime>
+//#include <Windows.h>
+#include <conio.h>
+#include "World.h"
+#include "OrganismMaker.h"
+
+using namespace std;
+
+#define SIZE 8
+
+int main()
+{
+	srand(time(NULL));
+
+	char tmpInput;
+	World* world;
+	while (true) 
+	{
+			world = new World(SIZE, SIZE);
+			world->generate_world();
+			break;
+	}
+
+	while (world->is_game_over() == false) {
+		world->drawWorld();
+		world->makeTurn();
+	}
+	delete world;
+	return 0;
+}
